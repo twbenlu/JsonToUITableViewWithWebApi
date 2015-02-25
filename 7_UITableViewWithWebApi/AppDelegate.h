60a7870f9d9a4945696ec1f40900aad34fd72a44
BenLu <twbenlu@gmail.com>
2015-02-25 08:49:48 +0800

    Initial Commit

---
+++ 7_UITableViewWithWebApi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  7_UITableViewWithWebApi
+//
+//  Created by benlu on 2/25/15.
+//  Copyright (c) 2015 benlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 7_UITableViewWithWebApi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  7_UITableViewWithWebApi
+//
+//  Created by benlu on 2/25/15.
+//  Copyright (c) 2015 benlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
